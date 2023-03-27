334fd2fd31f797ffbff7d5f708c0afcf04b2f60d
Stefan797 <stefanhubner97@web.de>
2023-03-27 22:17:30 +0200

    update 39

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  scroll(id: string){
+    document.getElementById(id).scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  }
 }
